1ab6f3acfee75d606ec9b91240121a71c050abed
赵泽武 <zhaozw5@163.com>
2021-07-07 14:25:47 +0800

    feat: u2net

--- api/controllers/upload_controller.go
+++ api/controllers/upload_controller.go
@@ -1,49 +1,53 @@
 package controllers
 
 import (
-    "comic/api/middleware"
-    "comic/common"
-    "comic/services"
-    "github.com/kataras/iris/v12"
-    "github.com/kataras/iris/v12/mvc"
-    "strconv"
+	"comic/api/middleware"
+	"comic/common"
+	"comic/services"
+	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/mvc"
+	"strconv"
 )
 
 type UploadController struct {
-    Ctx iris.Context
+	Ctx iris.Context
 }
 
 func (u *UploadController) BeforeActivation(b mvc.BeforeActivation) {
-    b.HandleMany("POST", "/transfer", "Transfer", middleware.AuthTokenHandler().Serve)
+	b.HandleMany("POST", "/transfer", "Transfer", middleware.AuthTokenHandler().Serve)
 }
 
 func (u *UploadController) Transfer() common.Response {
-    transferType, err := strconv.Atoi(u.Ctx.GetHeader("transfer_type"))
-    if err != nil {
-        return common.ReErrorMsg(err.Error())
-    }
+	transferTypes := u.Ctx.GetHeader("transferTypes")
+	if transferTypes == "" {
+		transferTypes = "1"
+	}
+	transferType, err := strconv.Atoi(transferTypes)
+	if err != nil {
+		return common.ReErrorMsg(err.Error())
+	}
 
-    file, _, err := u.Ctx.FormFile("file")
-    defer file.Close()
+	file, _, err := u.Ctx.FormFile("file")
+	defer file.Close()
 
-    if err != nil {
-        u.Ctx.StatusCode(iris.StatusInternalServerError)
-        u.Ctx.HTML("Error while uploading: <b>" + err.Error() + "</b>")
-        return common.ReErrorMsg(err.Error())
-    }
+	if err != nil {
+		u.Ctx.StatusCode(iris.StatusInternalServerError)
+		u.Ctx.HTML("Error while uploading: <b>" + err.Error() + "</b>")
+		return common.ReErrorMsg(err.Error())
+	}
 
-    user := middleware.ParseTokenToUser(u.Ctx)
+	user := middleware.ParseTokenToUser(u.Ctx)
 
-    service := services.NewUploadService()
-    path, err := service.Transfer(file, user.Id, transferType)
+	service := services.NewUploadService()
+	path, err := service.Transfer(file, user.Id, transferType)
 
-    if err != nil {
-        u.Ctx.StatusCode(iris.StatusInternalServerError)
-        u.Ctx.HTML("Error while uploading: <b>" + err.Error() + "</b>")
-        return common.ReErrorMsg(err.Error())
-    }
+	if err != nil {
+		u.Ctx.StatusCode(iris.StatusInternalServerError)
+		u.Ctx.HTML("Error while uploading: <b>" + err.Error() + "</b>")
+		return common.ReErrorMsg(err.Error())
+	}
 
-    return common.ReSuccessData(map[string]string{
-        "path": path,
-    })
+	return common.ReSuccessData(map[string]string{
+		"path": path,
+	})
 }
--- front/common/conifg.js
+++ front/common/conifg.js
@@ -3,17 +3,17 @@ console.log(`环境:${process.env.NODE_ENV}`);
 let domain;
 let environment = process.env.NODE_ENV;
 if (environment === 'development') {
-	domain = 'http://localhost:8081'
-} else {
-}
+	// domain = 'http://localhost:8081'
+	domain = 'http://comic.zwww.cool'
+} else {}
 
 const config = {
 	domain,
 
 	brand_id: 1,
-	
-	version:'v1.0.0',
-	
+
+	version: 'v1.0.0',
+
 	host: domain,
 
 	environment, //环境
